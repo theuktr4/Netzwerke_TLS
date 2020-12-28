16901d9a0be346f569089100af4bf24cc7dc8760
sebyk <seby.kiunke@googlemail.com>
2020-12-28 16:18:29 +0100

    added client & server with truststores

--- Client/src/TLSClient.java
+++ Client/src/TLSClient.java
@@ -1,12 +1,62 @@
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 public class TLSClient {
     private String tlsVersion;
     private String verschlüsselung;
     private final int PORT = 8080;
     private final int SIZE;
+    private SSLSocket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
+    public static void main(String[] args) {
+        TLSClient client = new TLSClient("test","1",25);
+    }
     public TLSClient(String version, String verschlüsselung, int SIZE) {
         this.tlsVersion = version;
         this.verschlüsselung = verschlüsselung;
         this.SIZE = SIZE;
+
+
+        try {
+
+            InputStream stream = this.getClass().getResourceAsStream("/sslclienttrust");
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            char[] trustStorePassword = "]3!z2Tb?@EHu%d}Q".toCharArray();
+            trustStore.load(stream, trustStorePassword);
+            SSLContext context = SSLContext.getInstance("TLSv1.3");
+            TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            factory.init(trustStore);
+            TrustManager[] managers = factory.getTrustManagers();
+            context.init(null, managers, null);
+            SSLContext.setDefault(context);
+            socket = (SSLSocket) context.getSocketFactory().createSocket("localhost", 8082);
+            try {
+                this.out = new ObjectOutputStream(this.socket.getOutputStream());
+            } catch (IOException var4) {
+                var4.printStackTrace();
+
+                try {
+                    this.out.close();
+                } catch (IOException var3) {
+                    var3.printStackTrace();
+                }
+            }
+
+        } catch (NoSuchAlgorithmException | KeyManagementException | IOException | KeyStoreException | CertificateException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/TLSServer.java
+++ Server/src/TLSServer.java
@@ -1,12 +1,83 @@
-public class TLSServer {
-    private String tlsVersion;
-    private String verschlüsselung;
-    private final int PORT = 8080;
-    private final int SIZE;
+import java.io.*;
+import java.security.KeyStore;
 
-    public TLSServer(String version, String verschlüsselung, int SIZE) {
-        this.tlsVersion = version;
-        this.verschlüsselung = verschlüsselung;
-        this.SIZE = SIZE;
+import javax.net.ssl.*;
+
+public class TLSServer {
+    private int port = 8082;
+    private boolean isServerDone = false;
+
+    public static void main(String[] args){
+        TLSServer server = new TLSServer();
+        server.run();
     }
+
+    TLSServer(){
+    }
+
+    TLSServer(String version, String encryption, int SIZE){
+    }
+
+    // Create the and initialize the SSLContext
+    private SSLContext createSSLContext(){
+        try{
+            InputStream stream = TLSServer.class.getResourceAsStream("/sslserverkeys");
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(stream,"7x*;^C(HU~5}@P?h".toCharArray());
+
+            // Create key manager
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(keyStore, "7x*;^C(HU~5}@P?h".toCharArray());
+            KeyManager[] km = keyManagerFactory.getKeyManagers();
+
+            // Create trust manager
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(keyStore);
+            TrustManager[] tm = trustManagerFactory.getTrustManagers();
+
+            // Initialize SSLContext
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.3");
+            sslContext.init(km,  tm, null);
+
+            return sslContext;
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    // Start to run the server
+    public void run(){
+        SSLContext sslContext = this.createSSLContext();
+
+        try{
+            // Create server socket factory
+            assert sslContext != null;
+            SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
+
+            // Create server socket
+            SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(this.port);
+            sslServerSocket.setEnabledCipherSuites(sslServerSocket.getSupportedCipherSuites());
+
+
+            System.out.println("SSL server started");
+            while(!isServerDone){
+                SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
+
+                // Start the server thread
+                sslSocket.startHandshake();
+                SSLSession sslSession = sslSocket.getSession();
+                System.out.println("SSLSession :");
+                System.out.println("\tProtocol : "+sslSession.getProtocol());
+                System.out.println("\tCipher suite : "+sslSession.getCipherSuite());
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+
+
 }
