4e80be20906f56084b5ac20f2ddfc18d54d9c38b
hm-haerting <lukas.haertinger@hm.edu>
2020-12-28 16:40:21 +0100

    KOnstruktor von Server

--- Server/src/TLSServer.java
+++ Server/src/TLSServer.java
@@ -1,2 +1,12 @@
 public class TLSServer {
+    private String tlsVersion;
+    private String verschlüsselung;
+    private final int PORT = 8080;
+    private final int SIZE;
+
+    public TLSServer(String version, String verschlüsselung, int SIZE) {
+        this.tlsVersion = version;
+        this.verschlüsselung = verschlüsselung;
+        this.SIZE = SIZE;
+    }
 }
