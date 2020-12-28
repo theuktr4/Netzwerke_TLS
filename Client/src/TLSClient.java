1dfc6fd28a78853d22a37dbc4bca5df87a05e57f
hm-haerting <lukas.haertinger@hm.edu>
2020-12-28 16:55:21 +0100

    Timer in Client

--- Client/src/TLSClient.java
+++ Client/src/TLSClient.java
@@ -3,10 +3,20 @@ public class TLSClient {
     private String verschlüsselung;
     private final int PORT = 8080;
     private final int SIZE;
+    private long time = 0;
 
     public TLSClient(String version, String verschlüsselung, int SIZE) {
         this.tlsVersion = version;
         this.verschlüsselung = verschlüsselung;
         this.SIZE = SIZE;
+    }
+
+    public void startTimer() {
+        this.time = System.currentTimeMillis();
+    }
+
+    public void stopTimer() {
+        System.out.println(time - System.currentTimeMillis());
+        this.time = 0;
     }
 }
